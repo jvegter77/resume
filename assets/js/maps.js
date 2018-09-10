b9a6843779b2f92238a3ec2325b422dba797032b
Jeroen Vegter <jeroenvegter@hotmail.com>
2018-09-10 16:58:16 +0000

    added gif loader and updated github-information.js with the search tool

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,7 +1,7 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 3,
-      center: {lat: 46.619261, lng: -33.134766}
+      center: {lat: 40.7143528, lng: -74.0059731}
     });
         
     var labels = "ABCDEFGHYJKLMNOPQRSTUVWXYZ";
