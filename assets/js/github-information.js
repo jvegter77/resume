3c0cdba2d4e41ddc4f03cdde893a44aeeae32d26
Jeroen Vegter <jeroenvegter@hotmail.com>
2018-09-10 20:03:32 +0000

    fixed bugs and errors in het github-information.js code finally works

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -2,16 +2,16 @@ function userInformationHTML(user) {
     return `
         <h2>${user.name}
             <span class="small-name">
-                (@< href="${user.html_url}" taget="_blank">${user.login}</a>)
+                (@<a href="${user.html_url}"target="_blank">${user.login}</a>)
             </span>
         </h2>
         <div class="gh-content">
             <div class="gh-avatar">
-                <a href="${user.htm_url} target="_blank">
+                <a href="${user.htm_url}" target="_blank">
                     <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}"/>
                 </a>
             </div>
-            <p>Followers: ${user.followers} - Following ${user.following} <br> Respos: ${user.public_repos}</p>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
         </div>`;
 }
 
@@ -19,7 +19,7 @@ function fetchGitHubInformation(event) {
     
     var username = $("#gh-username").val();
     if (!username) {
-        $("#gh-user-data").html(`<h2>Please enter a Github username</h2>`);
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
         return; 
     }
     
@@ -29,7 +29,7 @@ function fetchGitHubInformation(event) {
         </div>`);
         
     $.when(
-        $.getJSON('https://api.github.com/users/${username}'+ '?client_id=57261bf4094750b35a7b&client_secret=ce2841b62be5743268e021f2d5520c194313024e')
+        $.getJSON(`https://api.github.com/users/${username}`)
         ).then(
             function(response) {
                 var userData = response;
